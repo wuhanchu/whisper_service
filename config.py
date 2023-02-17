0577e798a6182c4e44c596ed90b78d66c7a6fea4
wuhanchu <whcwuhanchu@gmail.com>
2023-02-17 15:31:50 +0800

    修改

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     配置信息
     """
 
-    MODEL = os.getenv("MODEL", "base")  # 调用的模型
+    MODEL = os.getenv("MODEL", "medium")  # 调用的模型
     TEMP_FOLDER = os.getenv("TEMP_FOLDER", "items_temp")
